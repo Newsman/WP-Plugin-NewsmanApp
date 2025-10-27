f03b1391be0fc877255187123c8d57389ac15222
Daniel Ifrim <daniel@unifiedarts.de>
2025-10-27 22:13:15 +0200

    Default enabled newsletter checkout checkbox to subscribe to email list

--- includes/class-setup.php
+++ includes/class-setup.php
@@ -237,6 +237,7 @@ js/retargeting/modal_{{api_key}}.js'
 		add_option( 'newsman_api', 'on' );
 		add_option( 'newsman_useremarketing', 'on' );
 		add_option( 'newsman_remarketingsendtelephone', 'on' );
+		add_option( 'newsman_checkoutnewsletter', 'on' );
 		add_option( 'newsman_senduserip', 'on' );
 		add_option( 'newsman_developeractiveuserip', '' );
 		add_option( 'newsman_developeruserip', '' );
